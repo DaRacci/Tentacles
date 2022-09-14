b5710238bab8aea56c4a02cb6d364cb8d174797b
DaRacci <racci@racci.dev>
2022-09-14 15:43:49 +1000

    fix(Upstream): Hopefully fixes upstream workflow

--- scripts/upstreamCommit.sh
+++ scripts/upstreamCommit.sh
@@ -5,11 +5,14 @@
 # upstreamCommit <baseHash>
 # param: bashHash - the commit hash to use for comparing commits (baseHash...HEAD)
 
+function getCommits() {
+    curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$1"/compare/"$2"...HEAD | jq -r '.commits[] | "'"$1"'@\(.sha[:7]) \(.commit.message | split("\r\n")[0] | split("\n")[0])"'
+}
+
 (
 set -e
 PS1="$"
-
-purpur=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/PurpurMC/Purpur/compare/$1...HEAD | jq -r '.commits[] | "PurpurMC/Purpur@\(.sha[:7]) \(.commit.message | split("\r\n")[0] | split("\n")[0])"')
+purpur=$(getCommits "PurpurMC/Purpur" "$1")
 
 updated=""
 logsuffix=""
@@ -17,9 +20,9 @@ if [ -n "$purpur" ]; then
     logsuffix="$logsuffix\n\nPurpur Changes:\n$purpur"
     updated="Purpur"
 fi
-disclaimer="Upstream has released updates that appear to apply and compile correctly"
 
-log="${UP_LOG_PREFIX}Updated Upstream ($updated)\n\n${disclaimer}${logsuffix}"
+disclaimer="Upstream has released updates that appear to apply and compile correctly"
+log="Updated Upstream ($updated)\n\n${disclaimer}${logsuffix}"
 
 echo -e "$log" | git commit -F -
 
