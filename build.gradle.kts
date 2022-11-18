a7cf59efd4ba4caf3248c113873157fa2e361874
DaRacci <racci@racci.dev>
2022-11-18 14:21:28 +1100

    refactor: Build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,8 @@ plugins {
     id("io.papermc.paperweight.patcher") version "1.3.9"
 }
 
+val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
+
 repositories {
     mavenCentral()
     maven("https://papermc.io/repo/repository/maven-public/") {
@@ -22,14 +24,17 @@ dependencies {
     paperclip("io.papermc:paperclip:3.0.2")
 }
 
-subprojects {
+allprojects {
     apply(plugin = "java")
     apply(plugin = "maven-publish")
 
-    java {
-        toolchain {
-            languageVersion.set(JavaLanguageVersion.of(17))
-        }
+    repositories.mavenCentral()
+
+    java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+
+    publishing.repositories.maven("https://repo.racci.dev/snapshots") {
+        name = "RacciRepo"
+        credentials(PasswordCredentials::class)
     }
 }
 
@@ -48,23 +53,24 @@ subprojects {
     }
 
     repositories {
-        mavenCentral()
-        maven("https://oss.sonatype.org/content/groups/public/")
-        maven("https://papermc.io/repo/repository/maven-public/")
-        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
-        maven("https://repo.aikar.co/content/groups/aikar")
-        maven("https://repo.md-5.net/content/repositories/releases/")
-        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+        maven(paperMavenPublicUrl)
         maven("https://jitpack.io")
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    }
+}
+
+repositories {
+    maven(paperMavenPublicUrl) {
+        content {
+            onlyForConfigurations(configurations.paperclip.name)
+        }
     }
 }
 
 paperweight {
     serverProject.set(project(":tentacles-server"))
 
-    remapRepo.set("https://maven.fabricmc.net/")
-    decompileRepo.set("https://files.minecraftforge.net/maven/")
+    remapRepo.set(paperMavenPublicUrl)
+    decompileRepo.set(paperMavenPublicUrl)
 
     useStandardUpstream("purpur") {
         url.set(github("PurpurMC", "Purpur"))
@@ -89,23 +95,10 @@ tasks.generateDevelopmentBundle {
     libraryRepositories.set(
         listOf(
             "https://repo.maven.apache.org/maven2/",
-            "https://libraries.minecraft.net/",
-            "https://papermc.io/repo/repository/maven-public/",
-            "https://maven.quiltmc.org/repository/release/",
+            paperMavenPublicUrl,
             "https://repo.purpurmc.org/snapshots"
         )
     )
-}
-
-allprojects {
-    publishing {
-        repositories {
-            maven("https://repo.racci.dev/snapshots") {
-                name = "RacciRepo"
-                credentials(PasswordCredentials::class)
-            }
-        }
-    }
 }
 
 publishing {
